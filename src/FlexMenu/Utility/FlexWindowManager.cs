7cf42fb43c3be85096bfb73100caac6bbd825413
robhendriks <cyanidenl@icloud.com>
2015-12-07 22:44:18 +0100

    Initial window manager

---
+++ src/FlexMenu/Utility/FlexWindowManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FlexMenu.Utility
+{
+    public sealed class FlexWindowManager
+    {
+        #region Fields
+
+        private static FlexWindowManager _instance;
+
+        #endregion
+
+
+        #region Constructors
+
+        private FlexWindowManager()
+        {
+            // TODO
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        public static FlexWindowManager Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new FlexWindowManager());
+            }
+        }
+
+        #endregion
+
+    }
+}
