23a7c73c2b39ded0eb85fc881152cab4073453e8
robhendriks <cyanidenl@icloud.com>
2015-12-09 16:16:47 +0100

    Added menu groups

---
+++ src/FlexMenu/Controls/FlexMenuGroup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FlexMenu.Controls
+{
+    public class FlexMenuGroup : ItemsControl
+    {
+        #region Constructors
+
+        static FlexMenuGroup()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FlexMenuGroup)
+                , new FrameworkPropertyMetadata(typeof(FlexMenuGroup)));
+        }
+
+        #endregion
+
+        #region Properties
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title",
+            typeof(string), typeof(FlexMenuGroup), new PropertyMetadata());
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        #endregion
+    }
+}
