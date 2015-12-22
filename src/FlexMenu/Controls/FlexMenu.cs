9bf35c74e57f47681526c420d70db1cb2b999315
robhendriks <cyanidenl@icloud.com>
2015-12-22 16:03:39 +0100

    Bill gate'd :neckbeard:

--- src/FlexMenu/Controls/FlexMenu.cs
+++ src/FlexMenu/Controls/FlexMenu.cs
@@ -17,5 +17,28 @@ namespace FlexMenu.Controls
         }
 
         #endregion
+
+
+        #region Properties
+
+        public static readonly DependencyProperty PrependProperty = DependencyProperty.Register("Prepend"
+            , typeof(FrameworkElement), typeof(FlexMenu), new PropertyMetadata());
+
+        public FrameworkElement Prepend
+        {
+            get { return (FrameworkElement)GetValue(PrependProperty); }
+            set { SetValue(PrependProperty, value); }
+        }
+
+        public static readonly DependencyProperty AppendProperty = DependencyProperty.Register("Append"
+            , typeof(FrameworkElement), typeof(FlexMenu), new PropertyMetadata());
+
+        public FrameworkElement Append
+        {
+            get { return (FrameworkElement)GetValue(AppendProperty); }
+            set { SetValue(AppendProperty, value); }
+        }
+
+        #endregion
     }
 }
---
+++ src/FlexMenu/Controls/FlexUserBar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace FlexMenu.Controls
+{
+    public class FlexUserBar : Button
+    {
+        #region Constructors
+
+        static FlexUserBar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FlexUserBar)
+                , new FrameworkPropertyMetadata(typeof(FlexUserBar)));
+
+            // Image
+            var img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri("pack://application:,,,/FlexMenu;component/Resources/Bill.jpg");
+            img.EndInit();
+
+            IconProperty.OverrideMetadata(typeof(FlexUserBar)
+                , new FrameworkPropertyMetadata(img));
+        }
+
+        #endregion
+
+
+        #region Icon
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon",
+            typeof(ImageSource), typeof(FlexUserBar));
+
+        public ImageSource Icon
+        {
+            get { return (ImageSource)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        #endregion
+    }
+}
